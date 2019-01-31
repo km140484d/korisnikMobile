879c84418db99b12d0bc9df133f482f784d1475b
km183142m <mkonjikovac12@gmail.com>
2019-01-31 22:41:24 +0100

    Index menu designed and resources added.

--- app/src/main/java/com/example/mirjana/mobilekorisnik/LoginActivity.java
+++ app/src/main/java/com/example/mirjana/mobilekorisnik/LoginActivity.java
@@ -17,13 +17,13 @@ public class LoginActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.login_tab:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.registration_tab:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.index_search_tab:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
